25ace5ccdc58486286c764c523cb596aa646f3c4
Bedřich Schindler <bedrich@visionapps.cz>
2018-03-22 09:47:46 +0100

    fixup! fixup! fixup! Implement Symfony DI Container

--- src/TwigYard/Component/Application.php
+++ src/TwigYard/Component/Application.php
@@ -2,12 +2,8 @@
 
 namespace TwigYard\Component;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\DependencyInjection\Reference;
-use TwigYard\Exception\InvalidApplicationConfigException;
 use TwigYard\Middleware\Data\DataMiddleware;
 use TwigYard\Middleware\Error\ErrorMiddleware;
 use TwigYard\Middleware\Form\FormHandlerFactory;
@@ -20,11 +16,7 @@ use TwigYard\Middleware\Renderer\RendererMiddleware;
 use TwigYard\Middleware\Router\RouterMiddleware;
 use TwigYard\Middleware\Tracking\TrackingMiddleware;
 use TwigYard\Middleware\Url\UrlMiddleware;
-use Nette\Caching\Cache;
-use Nette\Caching\Storages\DevNullStorage;
-use Nette\Caching\Storages\FileStorage;
 use Relay\RelayBuilder;
-use Symfony\Component\Yaml\Yaml;
 use Zend\Diactoros\Response;
 use Zend\Diactoros\Server;
 use Zend\Diactoros\ServerRequestFactory;
@@ -57,7 +49,9 @@ class Application
     {
         $this->appRoot = $appRoot;
         $this->config = $config;
-        $this->container = $this->createContainer();
+
+        $containerFactory = new ContainerFactory($appRoot, $config);
+        $this->container = $containerFactory->createContainer();
     }
 
     public function run()
@@ -68,28 +62,6 @@ class Application
             new Response()
         );
         $server->listen();
-    }
-
-    /**
-     * @return ContainerBuilder
-     */
-    public function createContainer()
-    {
-        $containerBuilder = new ContainerBuilder();
-        $loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__));
-        $loader->load('../../../config/services.yml');
-
-        $this->registerConfigCacheService($containerBuilder, $this->getGlobalParameters());
-        $this->registerLoggerFactoryService($containerBuilder, $this->getGlobalParameters());
-
-        $containerBuilder->setParameter('app.root', $this->appRoot);
-        $containerBuilder->setParameter('app.config', $this->config);
-        $containerBuilder->setParameter('app.parameters', $this->getGlobalParameters());
-        $containerBuilder->setParameter('app.site_parameters', $this->getDefaultSiteParameters());
-
-        $containerBuilder->compile();
-
-        return $containerBuilder;
     }
 
     /**
@@ -107,28 +79,27 @@ class Application
     {
         $appState = new AppState();
 
-        $container = $this->container;
-        $globalParameters = $this->getGlobalParameters();
+        $globalParameters = $this->container->getParameter('app.parameters');
 
-        $formValidator = new FormValidator($container->get(ValidatorBuilderFactory::class));
+        $formValidator = new FormValidator($this->container->get(ValidatorBuilderFactory::class));
         $formHandlerFactory = new FormHandlerFactory(
             $appState,
-            $container->get(MailerFactory::class),
-            $container->get(TwigTemplatingFactory::class),
-            $container->get(SiteLoggerFactory::class)
+            $this->container->get(MailerFactory::class),
+            $this->container->get(TwigTemplatingFactory::class),
+            $this->container->get(SiteLoggerFactory::class)
         );
 
         $queue[] = new ErrorMiddleware(
             $appState,
             $globalParameters['show_errors'],
-            $container->get(LoggerFactory::class),
+            $this->container->get(LoggerFactory::class),
             $this->config->getTemplateDir(),
             $this->config->getError404PageName(),
             $this->config->getError500PageName()
         );
         $queue[] = new UrlMiddleware(
             $appState,
-            $container->get(ConfigCache::class),
+            $this->container->get(ConfigCache::class),
             $this->appRoot . '/' . $this->config->getSitesDir(),
             $globalParameters['site_config'],
             $this->config->getSiteParameters(),
@@ -138,105 +109,27 @@ class Application
         $queue[] = new RedirectMiddleware($appState);
         $queue[] = new HttpauthMiddleware($appState);
         $queue[] = new LocaleMiddleware($appState, self::VALID_LOCALES);
-        $queue[] = new DataMiddleware($appState, $this->config->getDataDir(), $container->get(CurlDownloader::class));
+        $queue[] = new DataMiddleware(
+            $appState,
+            $this->config->getDataDir(),
+            $this->container->get(CurlDownloader::class)
+        );
         $queue[] = new RouterMiddleware($appState);
         $queue[] = new FormMiddleware(
             $appState,
-            $container->get(CsrfTokenGenerator::class),
+            $this->container->get(CsrfTokenGenerator::class),
             $formValidator,
             $formHandlerFactory,
-            $container->get(TranslatorFactory::class),
-            $container->get(SiteTranslatorFactory::class),
+            $this->container->get(TranslatorFactory::class),
+            $this->container->get(SiteTranslatorFactory::class),
             $this->config->getLogDir()
         );
         $queue[] = new TrackingMiddleware($appState, $globalParameters['tracking_enabled']);
-        $queue[] = new RendererMiddleware($appState, $container->get(TwigTemplatingFactory::class));
+        $queue[] = new RendererMiddleware(
+            $appState,
+            $this->container->get(TwigTemplatingFactory::class)
+        );
 
         return $queue;
-    }
-
-    /**
-     * @return array
-     */
-    private function getGlobalParameters()
-    {
-        return Yaml::parse(
-            file_get_contents(
-                $this->appRoot . '/' . $this->config->getConfigDir() . '/' . $this->config->getGlobalParameters()
-            )
-        )['parameters'];
-    }
-
-    /**
-     * @return array
-     */
-    private function getDefaultSiteParameters()
-    {
-        return Yaml::parse(
-            file_get_contents(
-                $this->appRoot . '/' . $this->config->getConfigDir() . '/' . $this->config->getDefaultSiteParameters()
-            )
-        )['parameters'];
-    }
-
-    /**
-     * @param ContainerBuilder $containerBuilder
-     * @param array $parameters
-     * @return ConfigCache
-     */
-    private function registerConfigCacheService(ContainerBuilder $containerBuilder, array $parameters)
-    {
-        $cacheStorage = $parameters['cache_enabled']
-            ? new FileStorage($this->appRoot . '/' . $this->config->getConfigCacheDir())
-            : new DevNullStorage();
-        $cache = new Cache($cacheStorage, $this->config->getCacheNamespaceConfig());
-
-        $containerBuilder->register(ConfigCache::class)
-            ->setPublic(true)
-            ->addArgument($cache)
-            ->addArgument(new Reference(LoggerFactory::class));
-    }
-
-    /**
-     * @param ContainerBuilder $containerBuilder
-     * @param array $globalParameters
-     * @throws InvalidApplicationConfigException
-     */
-    private function registerLoggerFactoryService(ContainerBuilder $containerBuilder, array $globalParameters)
-    {
-        if (isset($globalParameters['log_rotation_enabled']) && !isset($globalParameters['log_max_files'])) {
-            throw new InvalidApplicationConfigException(
-                'If there is log_rotation_enabled defined in the configuration, log_max_files has to be defined too.'
-            );
-        }
-
-        if (isset($globalParameters['log_max_files']) && !isset($globalParameters['log_rotation_enabled'])) {
-            throw new InvalidApplicationConfigException(
-                'If there is log_max_files defined in the configuration, log_rotation_enabled has to be defined too.'
-            );
-        }
-
-        if (isset($globalParameters['loggly_token']) && !isset($globalParameters['loggly_tags'])) {
-            throw new InvalidApplicationConfigException(
-                'If there is loggly_token defined in the configuration, loggly_tags has to be defined too.'
-            );
-        }
-
-        if (isset($globalParameters['loggly_tags']) && !isset($globalParameters['loggly_token'])) {
-            throw new InvalidApplicationConfigException(
-                'If there is loggly_tags defined in the configuration, loggly_token has to be defined too.'
-            );
-        }
-
-        $containerBuilder->register(LoggerFactory::class)
-            ->setPublic(true)
-            ->addArgument($this->appRoot . '/' . $this->config->getLogDir())
-            ->addArgument(constant('Monolog\Logger::' . strtoupper($globalParameters['log_on_level'])))
-            ->addArgument(
-                isset($globalParameters['log_rotation_enabled']) ? $globalParameters['log_rotation_enabled'] : false
-            )
-            ->addArgument(isset($globalParameters['log_max_files']) ? $globalParameters['log_max_files'] : null)
-            ->addArgument(isset($globalParameters['loggly_token']) ? $globalParameters['loggly_token'] : null)
-            ->addArgument(isset($globalParameters['loggly_tags']) ? $globalParameters['loggly_tags'] : []);
     }
 }
--- src/TwigYard/Component/ContainerFactory.php
+++ src/TwigYard/Component/ContainerFactory.php
@@ -1,15 +1,150 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bedrich.schindler
- * Date: 21.03.18
- * Time: 21:01
- */
 
 namespace TwigYard\Component;
 
 
+use Nette\Caching\Cache;
+use Nette\Caching\Storages\DevNullStorage;
+use Nette\Caching\Storages\FileStorage;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Yaml\Yaml;
+use TwigYard\Exception\InvalidApplicationConfigException;
+
+
 class ContainerFactory
 {
+    /**
+     * @var string
+     */
+    private $appRoot;
 
+    /**
+     * @var ApplicationConfig
+     */
+    private $appConfig;
+
+    /**
+     * ContainerFactory constructor.
+     * @param $appRoot
+     * @param ApplicationConfig $appConfig
+     */
+    public function __construct($appRoot, ApplicationConfig $appConfig)
+    {
+        $this->appRoot = $appRoot;
+        $this->appConfig = $appConfig;
+    }
+
+    /**
+     * @return ContainerBuilder
+     */
+    public function createContainer()
+    {
+        $parameters = $this->getGlobalParameters();
+        $defaultSiteParameters = $this->getDefaultSiteParameters();
+
+        $containerBuilder = new ContainerBuilder();
+        $loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__));
+        $loader->load('../../../config/services.yml');
+
+        $this->registerConfigCacheService($containerBuilder, $parameters);
+        $this->registerLoggerFactoryService($containerBuilder, $parameters);
+
+        $containerBuilder->setParameter('app.root', $this->appRoot);
+        $containerBuilder->setParameter('app.config', $this->appConfig);
+        $containerBuilder->setParameter('app.parameters', $parameters);
+        $containerBuilder->setParameter('app.site_parameters', $defaultSiteParameters);
+
+        $containerBuilder->compile();
+
+        return $containerBuilder;
+    }
+
+    /**
+     * @param ContainerBuilder $containerBuilder
+     * @param array $parameters
+     * @return ConfigCache
+     */
+    private function registerConfigCacheService(ContainerBuilder $containerBuilder, array $parameters)
+    {
+        $cacheStorage = $parameters['cache_enabled']
+            ? new FileStorage($this->appRoot . '/' . $this->appConfig->getConfigCacheDir())
+            : new DevNullStorage();
+        $cache = new Cache($cacheStorage, $this->appConfig->getCacheNamespaceConfig());
+
+        $containerBuilder->register(ConfigCache::class)
+            ->setPublic(true)
+            ->addArgument($cache)
+            ->addArgument(new Reference(LoggerFactory::class));
+    }
+
+    /**
+     * @param ContainerBuilder $containerBuilder
+     * @param array $globalParameters
+     * @throws InvalidApplicationConfigException
+     */
+    private function registerLoggerFactoryService(ContainerBuilder $containerBuilder, array $globalParameters)
+    {
+        if (isset($globalParameters['log_rotation_enabled']) && !isset($globalParameters['log_max_files'])) {
+            throw new InvalidApplicationConfigException(
+                'If there is log_rotation_enabled defined in the configuration, log_max_files has to be defined too.'
+            );
+        }
+
+        if (isset($globalParameters['log_max_files']) && !isset($globalParameters['log_rotation_enabled'])) {
+            throw new InvalidApplicationConfigException(
+                'If there is log_max_files defined in the configuration, log_rotation_enabled has to be defined too.'
+            );
+        }
+
+        if (isset($globalParameters['loggly_token']) && !isset($globalParameters['loggly_tags'])) {
+            throw new InvalidApplicationConfigException(
+                'If there is loggly_token defined in the configuration, loggly_tags has to be defined too.'
+            );
+        }
+
+        if (isset($globalParameters['loggly_tags']) && !isset($globalParameters['loggly_token'])) {
+            throw new InvalidApplicationConfigException(
+                'If there is loggly_tags defined in the configuration, loggly_token has to be defined too.'
+            );
+        }
+
+        $containerBuilder->register(LoggerFactory::class)
+            ->setPublic(true)
+            ->addArgument($this->appRoot . '/' . $this->appConfig->getLogDir())
+            ->addArgument(constant('Monolog\Logger::' . strtoupper($globalParameters['log_on_level'])))
+            ->addArgument(
+                isset($globalParameters['log_rotation_enabled']) ? $globalParameters['log_rotation_enabled'] : false
+            )
+            ->addArgument(isset($globalParameters['log_max_files']) ? $globalParameters['log_max_files'] : null)
+            ->addArgument(isset($globalParameters['loggly_token']) ? $globalParameters['loggly_token'] : null)
+            ->addArgument(isset($globalParameters['loggly_tags']) ? $globalParameters['loggly_tags'] : []);
+    }
+
+
+    /**
+     * @return array
+     */
+    private function getGlobalParameters()
+    {
+        return Yaml::parse(
+            file_get_contents(
+                $this->appRoot . '/' . $this->appConfig->getConfigDir() . '/' . $this->appConfig->getGlobalParameters()
+            )
+        )['parameters'];
+    }
+
+    /**
+     * @return array
+     */
+    private function getDefaultSiteParameters()
+    {
+        return Yaml::parse(
+            file_get_contents(
+                $this->appRoot . '/' . $this->appConfig->getConfigDir() . '/' . $this->appConfig->getDefaultSiteParameters()
+            )
+        )['parameters'];
+    }
 }
